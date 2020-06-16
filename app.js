1876c7ac7e4305ef0be77a838214f7e194bdefad
Derek Larm <dlarm112@yahoo.com>
2020-06-16 17:16:29 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,152 @@
+const fs = require('fs')
+const yargs = require('yargs')
+const chalk = require('chalk');
+const {
+    describe
+} = require('yargs');
+const {
+    clear
+} = require('console');
+
+function loadData() {
+    try {
+        const buffer = fs.readFileSync('data.json')
+        const data = buffer.toString()
+        const dataObj = JSON.parse(data)
+        return dataObj
+    } catch (err) {
+        return []
+    }
+}
+
+function saveData(data) {
+    fs.writeFileSync('data.json', JSON.stringify(data))
+}
+
+function addTodo(todo, status) {
+    const data = loadData()
+    const newTodo = {
+        todo: todo,
+        status: status
+    }
+    data.push(newTodo)
+    saveData(data)
+}
+
+yargs.command({
+    command: "list",
+    describe: "List all todos",
+    builder: {
+        status: {
+            describe: "status of todos",
+            default: "all",
+            type: "string",
+            alias: "s",
+        },
+    },
+    handler: function (arg) {
+        console.log(chalk.blue.bold("listing todos"))
+
+        console.log(arg)
+        if (arg.status === "all") {
+            data = data
+            console.log("all wokred")
+        } else if (arg.status === "list complete") {
+            data = data.filter(item => item.status === true)
+        } else if (arg.status === "list incomplete") {
+            data = data.filter(item => item.status === false)
+        }
+        data.forEach(({
+                todo,
+                status
+            }, idx) =>
+            console.log(`idx: ${idx} todo: ${todo} status: ${status}`))
+    },
+})
+
+yargs.command({
+    command: "delete",
+    describe: "deletes single index",
+    builder: {
+        id: {
+            demandOption: true,
+            type: "string",
+            alias: "i",
+        },
+    },
+    handler: function (arg) {
+        let data = loadData()
+        if (arg.id === "all") {
+            data = []
+            saveData(data)
+        } else {
+            data.splice(arg.id, 1)
+            saveData(data)
+        }
+
+    },
+})
+
+yargs.command({
+    command: "add",
+    describe: "add new todo",
+    builder: {
+        todo: {
+            describe: "todo content",
+            demandOption: true,
+            type: "string",
+            alias: "t"
+        },
+        status: {
+            describe: "status of todo",
+            demandOption: false,
+            type: "boolean",
+            alias: "s",
+            default: false,
+        },
+    },
+    handler: function ({
+        todo,
+        status
+    }) {
+        addTodo(todo, status)
+        console.log("added todo succesful")
+    }
+})
+
+yargs.command({
+    command: "toggle",
+    describe: "completes single index",
+    builder: {
+        id: {
+            demandOption: true,
+            type: "string",
+            alias: "t",
+        },
+    },
+    handler: function (arg) {
+        let data = loadData()
+        console.log(data)
+        data[arg.id].status = data[arg.id].status != true
+        saveData(data)
+    }
+})
+
+
+
+
+
+
+
+yargs.command({
+    command: "delete_all",
+    describe: "deletes all todos",
+    handler: function () {
+        let data = []
+        saveData(data)
+    }
+})
+
+
+
+yargs.parse()
